5b42962178d0d576340cd19c4cff7987c587f684
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2024-01-30 19:56:12 -0500

    env database

--- src/repositories/turso/turso-create.js
+++ src/repositories/turso/turso-create.js
@@ -2,33 +2,31 @@ const { createClient } = require("@libsql/client");
 require("dotenv").config();
 
 const token = process.env.TURSO_TOKEN_API;
+const tursoDatabaseUrl = process.env.TURSO_DATABASE_URL;
 
 // Inicializar el cliente de Turso DB
 const client = createClient({
-  url: "libsql://whatsapp-1-brareyesb15.turso.io",
+  url: tursoDatabaseUrl,
   // authToken es necesario si se conecta a una base de datos remota de Turso DB
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIyMDI0LTAxLTI2VDE5OjIxOjI3Ljg4NjY3MTMwOVoiLCJpZCI6IjA0ODMyZDhmLWJjNzktMTFlZS1hYmZmLWUyZTgyZWFhY2IwNSJ9.VrD3yNngGNahOJgkaBxn129Aa8diYmZ3MoNm1-eH_Xuk0J-0JO0iBTNmbfaeiQx04Gf_epYK8TQLteRdGG7SAw",
+  authToken: token,
 });
 
 // Función para crear la tabla si no existe
 async function initializeDatabase() {
   try {
-    const createTableSQL = `
-      CREATE TABLE IF NOT EXISTS messages2 (
-        messageId INTEGER PRIMARY KEY AUTOINCREMENT,
-        botId INTEGER NOT NULL,
-        agentId INTEGER NOT NULL,
-        timestamp DATETIME NOT NULL,
-        phoneNumber TEXT NOT NULL,
-        content TEXT NOT NULL,
-        role TEXT NOT NULL
-      )
-    `;
-
-    await client.execute(createTableSQL);
-    console.log('Table "mensajes" has been created or already exists.');
-    return 'Table "mensajes" has been created or already exists.';
+    // const createTableSQL = `
+    //   CREATE TABLE IF NOT EXISTS messages2 (
+    //     messageId INTEGER PRIMARY KEY AUTOINCREMENT,
+    //     botId INTEGER NOT NULL,
+    //     agentId INTEGER NOT NULL,
+    //     timestamp DATETIME NOT NULL,
+    //     phoneNumber TEXT NOT NULL,
+    //     content TEXT NOT NULL,
+    //     role TEXT NOT NULL
+    //   )
+    // `;
+    // await client.execute(createTableSQL);
+    // console.log('New table "messages2" has been created.');
   } catch (error) {
     console.error(
       "An error occurred while initializing the database:",
@@ -36,7 +34,6 @@ async function initializeDatabase() {
     );
   }
 }
-
 module.exports = {
   initializeDatabase,
 };
--- src/repositories/turso/turso-repository.js
+++ src/repositories/turso/turso-repository.js
@@ -2,13 +2,13 @@ const { createClient } = require("@libsql/client");
 require("dotenv").config();
 
 const token = process.env.TURSO_TOKEN_API;
+const tursoDatabaseUrl = process.env.TURSO_DATABASE_URL;
 
 // Inicializar el cliente de Turso DB
 const client = createClient({
-  url: "libsql://whatsapp-1-brareyesb15.turso.io",
+  url: tursoDatabaseUrl,
   // authToken es necesario si se conecta a una base de datos remota de Turso DB
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIyMDI0LTAxLTI2VDE5OjIxOjI3Ljg4NjY3MTMwOVoiLCJpZCI6IjA0ODMyZDhmLWJjNzktMTFlZS1hYmZmLWUyZTgyZWFhY2IwNSJ9.VrD3yNngGNahOJgkaBxn129Aa8diYmZ3MoNm1-eH_Xuk0J-0JO0iBTNmbfaeiQx04Gf_epYK8TQLteRdGG7SAw",
+  authToken: token,
 });
 
 // Función para insertar un mensaje en la base de datos
