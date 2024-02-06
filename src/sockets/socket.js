178cc30bee60d26f525900d0a120b9b533e10340
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2024-02-06 12:24:44 -0500

    try catch sockets

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -5,51 +5,58 @@ const { instanciasBot } = require("../general-configs/instances");
 const { chatbotOff, chatbotKill } = require("./socketControllers/chatbotOff");
 require("dotenv").config();
 
-const frontendProd = process.env.FRONTEND_PROD;
-
-eventEmitter.on("qrRemoved", (number, sessionName) => {
-  quitarQr(number, sessionName);
-});
-
-eventEmitter.on("qrCreated", (qr, sessionName) => {
-  createQr(qr, sessionName);
-});
-
 let io;
 
-/**
- * Configura el socket para el servidor.
- * @param {http.Server} server - El servidor HTTP al que se asocia Socket.IO.
- */
 const configureSocket = async (server) => {
-  // Crear una instancia de Socket.IO asociada al servidor
-  io = socketIO(server, {
-    cors: {
-      origin: "*", // mandar esto a .env como "frontend-url"
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
-  });
-  io.on("connection", async (socket) => {
-    socket.on("killBot", async (data) => {
-      console.log("Entró a killBot:", data.apiKey);
-      chatbotKill(data.apiKey);
+  try {
+    io = socketIO(server, {
+      cors: {
+        origin: "*", // mandar esto a .env como "frontend-url"
+        methods: ["GET", "POST"],
+        credentials: true,
+      },
     });
-    socket.on("enviarDatos", async (data) => {
-      console.log("Data connection", data);
-      if (data) {
-        socket.join(data.apiKey); // Unir el socket a una sala con el nombre de apiKey
-        socket.apiKey = data.apiKey;
-        await chatbotOn(data.apiKey, data.agentId, data.agentState);
-        instanciasBot[data.apiKey].qr
-          ? io.to(data.apiKey).emit("qr", instanciasBot[data.apiKey].qr)
-          : io
-              .to(data.apiKey)
-              .emit("qr", +instanciasBot[data.apiKey].botNumber?.split("@")[0]);
 
-        instanciasBot[data.apiKey]?.frontendConnection = true;
+    io.on("connection", async (socket) => {
+      socket.on("killBot", async (data) => {
+        try {
+          console.log("Entró a killBot:", data.apiKey);
+          await chatbotKill(data.apiKey);
+        } catch (error) {
+          console.error(
+            `Error al matar el bot con apiKey: ${data.apiKey}`,
+            error
+          );
+        }
+      });
 
-        socket.on("disconnect", () => {
+      socket.on("enviarDatos", async (data) => {
+        try {
+          console.log("Data connection", data);
+          if (data && instanciasBot[data.apiKey]) {
+            socket.join(data.apiKey);
+            socket.apiKey = data.apiKey;
+            await chatbotOn(data.apiKey, data.agentId, data.agentState);
+            const qrData =
+              instanciasBot[data.apiKey].qr ||
+              instanciasBot[data.apiKey].botNumber?.split("@")[0];
+            io.to(data.apiKey).emit("qr", qrData);
+            instanciasBot[data.apiKey].frontendConnection = true;
+          } else {
+            console.error(
+              `No se encontraron datos válidos o instancia para apiKey: ${data.apiKey}`
+            );
+          }
+        } catch (error) {
+          console.error(
+            `Error al procesar los datos enviados: ${data.apiKey}`,
+            error
+          );
+        }
+      });
+
+      socket.on("disconnect", () => {
+        try {
           if (instanciasBot && instanciasBot.hasOwnProperty(data.apiKey)) {
             instanciasBot[data.apiKey].frontendConnection = false;
           } else {
@@ -59,19 +66,34 @@ const configureSocket = async (server) => {
           }
           console.log("Un cliente se ha desconectado");
           chatbotOff(data.apiKey);
-        });
-      }
+        } catch (error) {
+          console.error(
+            `Error al manejar la desconexión para apiKey: ${data.apiKey}`,
+            error
+          );
+        }
+      });
     });
-  });
+  } catch (error) {
+    console.error("Error al configurar Socket.IO:", error);
+  }
 };
 
 const createQr = async (qr, name) => {
-  io.to(name).emit("qr", qr);
+  try {
+    io.to(name).emit("qr", qr);
+  } catch (error) {
+    console.error(`Error al crear QR para la sesión: ${name}`, error);
+  }
 };
 
 const quitarQr = async (number, name) => {
-  number = +number;
-  io.to(name).emit("qr", number);
+  try {
+    number = +number;
+    io.to(name).emit("qr", number);
+  } catch (error) {
+    console.error(`Error al quitar QR para la sesión: ${name}`, error);
+  }
 };
 
 module.exports = {
